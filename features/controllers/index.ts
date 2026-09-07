bda7ba5dd6fb96da42d6ae98d3b7ed8fca78eb60
simon9703 <1138864681@qq.com>
2026-09-07 10:44:33 +0800

    feat: export controller generation contracts

---
+++ features/controllers/index.ts
@@ -0,0 +1,3 @@
+export { CONTROLLER_TASK_PROMPT, buildControllerPrompt } from './prompt';
+export { controllerStrategySchema, controllerSubmissionSchema, parseControllerSubmission } from './spec';
+export type { ControllerStrategyManifest, ControllerSubmission } from './spec';
