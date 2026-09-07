b58280aba7dd99cda1a01d72a670c54d03d7559c
simon9703 <1138864681@qq.com>
2026-09-07 10:44:50 +0800

    feat: export controller identity helpers

--- features/controllers/index.ts
+++ features/controllers/index.ts
@@ -1,3 +1,4 @@
 export { CONTROLLER_TASK_PROMPT, buildControllerPrompt } from './prompt';
 export { controllerStrategySchema, controllerSubmissionSchema, parseControllerSubmission } from './spec';
+export { createControllerId, createSourceHash } from './identity';
 export type { ControllerStrategyManifest, ControllerSubmission } from './spec';
