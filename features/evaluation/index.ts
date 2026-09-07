ba04463b891a9943e78547443485df1d8cfea12e
simon9703 <1138864681@qq.com>
2026-09-07 12:42:48 +0800

    feat: export tournament reporting helpers

--- features/evaluation/index.ts
+++ features/evaluation/index.ts
@@ -3,4 +3,5 @@ export { createControllerLock, parseControllerLock } from './controller-lock';
 export type { ControllerLock, LockedController } from './controller-lock';
 export { createTournamentArtifact, serializeTournamentArtifact } from './tournament-artifact';
 export type { TournamentArtifact } from './tournament-artifact';
+export { createTournamentReport } from './report';
 export type { EvaluateSubmissionsInput, SubmissionTournamentResult } from './submission-tournament';
