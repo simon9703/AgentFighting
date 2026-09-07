c18bf6ed07bd6444ce18eb558a632000ca636e6d
simon9703 <1138864681@qq.com>
2026-09-07 10:45:54 +0800

    feat: export recorded match runner

--- features/replay/index.ts
+++ features/replay/index.ts
@@ -1,3 +1,5 @@
 export { createReplayRecorder } from './recorder';
+export { runRecordedMatch } from './run-recorded-match';
 export { REPLAY_SCHEMA_VERSION } from './types';
+export type { RunRecordedMatchInput } from './run-recorded-match';
 export type * from './types';
