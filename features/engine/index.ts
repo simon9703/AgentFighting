18c5548a9e157370e4b6e2f1328bddca9444d4f9
simon9703 <1138864681@qq.com>
2026-09-07 10:27:43 +0800

    feat: export headless engine API

---
+++ features/engine/index.ts
@@ -0,0 +1,4 @@
+export { createArenaEngine, DEFAULT_CONFIG } from './engine';
+export { createObservation } from './observation';
+export { sanitizeAction, IDLE_ACTION } from './actions';
+export type * from './types';
