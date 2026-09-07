61bd138a185e59173a7cd6920e35df423b7bafee
simon9703 <1138864681@qq.com>
2026-09-07 13:25:56 +0800

    refactor: export reusable tournament aggregation

--- features/engine/index.ts
+++ features/engine/index.ts
@@ -1,6 +1,6 @@
 export { createArenaEngine, DEFAULT_CONFIG } from './engine';
 export { createObservation } from './observation';
 export { sanitizeAction, IDLE_ACTION } from './actions';
-export { runTournament } from './tournament';
+export { runTournament, aggregateTournamentSummaries } from './tournament';
 export type * from './types';
 export type { BehaviorFingerprint, TournamentAgentResult, TournamentResult } from './tournament';
